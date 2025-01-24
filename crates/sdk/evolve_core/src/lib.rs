bbb7d84e2a36718961e00bce099589479d96e64b
testinginprod <98415576+testinginprod@users.noreply.github.com>
2025-01-24 13:42:18 +0100

    add more things

--- crates/sdk/evolve_core/src/lib.rs
+++ crates/sdk/evolve_core/src/lib.rs
@@ -1,6 +1,8 @@
 pub type ErrorCode = u64;
 
-pub struct AccountId (u128);
+pub type SdkResult<T> = Result<T, ErrorCode>;
+
+pub struct AccountId(u128);
 
 enum InnerMessage {
     OwnedBytes(Vec<u8>),
@@ -9,13 +11,13 @@ enum InnerMessage {
 /// Defines a message, the internals of this type are hidden such that we can improve them later
 /// for performance.
 pub struct Message {
-    inner: InnerMessage
+    inner: InnerMessage,
 }
 
 impl From<Vec<u8>> for Message {
     fn from(bytes: Vec<u8>) -> Self {
-        Message{
-            inner: InnerMessage::OwnedBytes(bytes)
+        Message {
+            inner: InnerMessage::OwnedBytes(bytes),
         }
     }
 }
@@ -30,7 +32,7 @@ pub struct InvokeRequest {
 
 impl InvokeRequest {
     pub fn new(function_identifier: u64, message: Message) -> Self {
-        InvokeRequest{
+        InvokeRequest {
             function_identifier,
             message,
         }
@@ -39,34 +41,52 @@ impl InvokeRequest {
 
 /// Defines the response of an [`InvokeRequest`]
 pub struct InvokeResponse {
-    response: Result<Message, ErrorCode>,
+    response: Message,
 }
 
-
 impl InvokeResponse {
-    pub fn new(response: Result<Message, ErrorCode>) -> Self {
-        InvokeResponse {
-            response,
-        }
+    pub fn new(response: Message) -> Self {
+        InvokeResponse { response }
     }
+}
 
-    pub fn new_error(err_code: ErrorCode) -> Self {
-        Self::new(Err(err_code))
-    }
-
-    pub fn new_response(response: Result<Message, ErrorCode>) -> Self {
-        InvokeResponse::new(response)
-    }
+pub trait Invoker {
+    fn do_query(
+        &self,
+        ctx: &Context,
+        to: AccountId,
+        data: InvokeRequest,
+    ) -> SdkResult<InvokeResponse>;
+    fn do_exec(
+        &mut self,
+        ctx: &mut Context,
+        to: AccountId,
+        data: InvokeRequest,
+    ) -> SdkResult<InvokeResponse>;
 }
 
 /// Defines the execution context.
 pub struct Context {
     whoami: AccountId,
+    sender: AccountId,
 }
 
 /// Defines some arbitrary code that can handle account execution logic.
 pub trait AccountCode {
-    fn init(ctx: &mut Context, request: InvokeRequest) -> InvokeResponse;
-    fn execute(ctx: &mut Context, request: InvokeRequest) -> InvokeResponse;
-    fn query(ctx: &Context, request: InvokeRequest) -> InvokeResponse;
+    fn identifier(&self) -> &'static str;
+    fn init<I: Invoker>(
+        invoker: &mut I,
+        ctx: &mut Context,
+        request: InvokeRequest,
+    ) -> SdkResult<InvokeResponse>;
+    fn execute<I: Invoker>(
+        invoker: &mut I,
+        ctx: &mut Context,
+        request: InvokeRequest,
+    ) -> SdkResult<InvokeResponse>;
+    fn query<I: Invoker>(
+        invoker: &I,
+        ctx: &Context,
+        request: InvokeRequest,
+    ) -> SdkResult<InvokeResponse>;
 }
